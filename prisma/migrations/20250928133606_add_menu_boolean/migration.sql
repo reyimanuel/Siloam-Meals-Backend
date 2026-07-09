77053eae2a68beaa208aa383d33b9beefcd7a29e
Miracle Sumajow <miraclesumajow32@gmail.com>
2026-07-09 20:22:04 +0800

    moving to cloud

--- prisma/migrations/20250928133606_add_menu_boolean/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."Makanan" ADD COLUMN     "isPaket" BOOLEAN NOT NULL DEFAULT false;
--- prisma/migrations/20251007043237_hamdalah/migration.sql
+++ prisma/migrations/20251007043237_hamdalah/migration.sql
@@ -7,6 +7,9 @@ CREATE TYPE "public"."Role" AS ENUM ('NURSE', 'KITCHEN', 'ADMIN', 'DIETISIEN');
 -- CreateEnum
 CREATE TYPE "public"."Status" AS ENUM ('ACTIVE', 'INACTIVE', 'PENDING');
 
+-- CreateEnum
+CREATE TYPE "public"."StatusPesanan" AS ENUM ('PENDING', 'SELESAI', 'DITERIMA', 'BATAL');
+
 -- CreateTable
 CREATE TABLE "public"."User" (
     "idUser" SERIAL NOT NULL,
@@ -23,9 +26,11 @@ CREATE TABLE "public"."User" (
 CREATE TABLE "public"."Pasien" (
     "idPasien" SERIAL NOT NULL,
     "uuid" TEXT NOT NULL,
+    "noKtp" TEXT,
+    "tanggalLahir" TIMESTAMP(3),
     "mr" TEXT NOT NULL,
     "namaPasien" TEXT NOT NULL,
-    "tempatTidur" TEXT NOT NULL,
+    "ruanganInap" TEXT NOT NULL DEFAULT 'default_value',
     "diagnosa" TEXT NOT NULL,
     "status" "public"."Status" NOT NULL,
     "validate" BOOLEAN NOT NULL DEFAULT false,
@@ -39,11 +44,21 @@ CREATE TABLE "public"."Pasien" (
 );
 
 -- CreateTable
+CREATE TABLE "public"."PengecualianMakanan" (
+    "id" SERIAL NOT NULL,
+    "pasienId" INTEGER NOT NULL,
+    "makananId" INTEGER NOT NULL,
+
+    CONSTRAINT "PengecualianMakanan_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "public"."Makanan" (
     "idMakanan" SERIAL NOT NULL,
     "namaMakanan" TEXT NOT NULL,
     "jenis" "public"."Jenis" NOT NULL,
     "gambar" TEXT,
+    "isPaket" BOOLEAN NOT NULL DEFAULT false,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
     "menuId" INTEGER,
@@ -87,8 +102,13 @@ CREATE TABLE "public"."Pantangan" (
 -- CreateTable
 CREATE TABLE "public"."Pesanan" (
     "idPesanan" SERIAL NOT NULL,
-    "pasienId" INTEGER NOT NULL,
+    "pasienId" INTEGER,
     "sesi" TEXT NOT NULL,
+    "namaPasienHistory" TEXT,
+    "ruanganInapHistory" TEXT,
+    "mrHistory" TEXT,
+    "diagnosaHistory" TEXT,
+    "status" "public"."StatusPesanan" NOT NULL DEFAULT 'PENDING',
     "tanggal" TIMESTAMP(3) NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
@@ -100,9 +120,24 @@ CREATE TABLE "public"."Pesanan" (
 CREATE TABLE "public"."PesananDetail" (
     "idPesananDetail" SERIAL NOT NULL,
     "pesananId" INTEGER NOT NULL,
-    "makananId" INTEGER NOT NULL,
+    "makananId" INTEGER,
+    "namaMakananHistory" TEXT,
+    "jenisHistory" "public"."Jenis",
 
     CONSTRAINT "PesananDetail_pkey" PRIMARY KEY ("idPesananDetail")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Feedback" (
+    "idFeedback" SERIAL NOT NULL,
+    "pasienId" INTEGER NOT NULL,
+    "pengirimId" INTEGER NOT NULL,
+    "penerimaId" INTEGER NOT NULL,
+    "pesan" TEXT NOT NULL,
+    "isResolved" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Feedback_pkey" PRIMARY KEY ("idFeedback")
 );
 
 -- CreateTable
@@ -126,10 +161,16 @@ CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
 CREATE UNIQUE INDEX "Pasien_uuid_key" ON "public"."Pasien"("uuid");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "Pasien_noKtp_key" ON "public"."Pasien"("noKtp");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "Pasien_mr_key" ON "public"."Pasien"("mr");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Pasien_link_key" ON "public"."Pasien"("link");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "PengecualianMakanan_pasienId_makananId_key" ON "public"."PengecualianMakanan"("pasienId", "makananId");
 
 -- CreateIndex
 CREATE INDEX "_UtamaRelasi_B_index" ON "public"."_UtamaRelasi"("B");
@@ -141,31 +182,46 @@ ALTER TABLE "public"."Pasien" ADD CONSTRAINT "Pasien_createdBy_fkey" FOREIGN KEY
 ALTER TABLE "public"."Pasien" ADD CONSTRAINT "Pasien_validatedBy_fkey" FOREIGN KEY ("validatedBy") REFERENCES "public"."User"("idUser") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "public"."PengecualianMakanan" ADD CONSTRAINT "PengecualianMakanan_pasienId_fkey" FOREIGN KEY ("pasienId") REFERENCES "public"."Pasien"("idPasien") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."PengecualianMakanan" ADD CONSTRAINT "PengecualianMakanan_makananId_fkey" FOREIGN KEY ("makananId") REFERENCES "public"."Makanan"("idMakanan") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "public"."Makanan" ADD CONSTRAINT "Makanan_createdBy_fkey" FOREIGN KEY ("createdBy") REFERENCES "public"."User"("idUser") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "public"."Makanan" ADD CONSTRAINT "Makanan_menuId_fkey" FOREIGN KEY ("menuId") REFERENCES "public"."Menu"("idMenu") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "public"."TanggalTersedia" ADD CONSTRAINT "TanggalTersedia_makananId_fkey" FOREIGN KEY ("makananId") REFERENCES "public"."Makanan"("idMakanan") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "public"."TanggalTersedia" ADD CONSTRAINT "TanggalTersedia_makananId_fkey" FOREIGN KEY ("makananId") REFERENCES "public"."Makanan"("idMakanan") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "public"."Menu" ADD CONSTRAINT "Menu_createdBy_fkey" FOREIGN KEY ("createdBy") REFERENCES "public"."User"("idUser") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "public"."Pantangan" ADD CONSTRAINT "Pantangan_makananId_fkey" FOREIGN KEY ("makananId") REFERENCES "public"."Makanan"("idMakanan") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "public"."Pantangan" ADD CONSTRAINT "Pantangan_makananId_fkey" FOREIGN KEY ("makananId") REFERENCES "public"."Makanan"("idMakanan") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "public"."Pantangan" ADD CONSTRAINT "Pantangan_pasienId_fkey" FOREIGN KEY ("pasienId") REFERENCES "public"."Pasien"("idPasien") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "public"."Pesanan" ADD CONSTRAINT "Pesanan_pasienId_fkey" FOREIGN KEY ("pasienId") REFERENCES "public"."Pasien"("idPasien") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "public"."Pesanan" ADD CONSTRAINT "Pesanan_pasienId_fkey" FOREIGN KEY ("pasienId") REFERENCES "public"."Pasien"("idPasien") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "public"."PesananDetail" ADD CONSTRAINT "PesananDetail_pesananId_fkey" FOREIGN KEY ("pesananId") REFERENCES "public"."Pesanan"("idPesanan") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "public"."PesananDetail" ADD CONSTRAINT "PesananDetail_makananId_fkey" FOREIGN KEY ("makananId") REFERENCES "public"."Makanan"("idMakanan") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "public"."PesananDetail" ADD CONSTRAINT "PesananDetail_makananId_fkey" FOREIGN KEY ("makananId") REFERENCES "public"."Makanan"("idMakanan") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Feedback" ADD CONSTRAINT "Feedback_pasienId_fkey" FOREIGN KEY ("pasienId") REFERENCES "public"."Pasien"("idPasien") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Feedback" ADD CONSTRAINT "Feedback_pengirimId_fkey" FOREIGN KEY ("pengirimId") REFERENCES "public"."User"("idUser") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Feedback" ADD CONSTRAINT "Feedback_penerimaId_fkey" FOREIGN KEY ("penerimaId") REFERENCES "public"."User"("idUser") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "public"."_UtamaRelasi" ADD CONSTRAINT "_UtamaRelasi_A_fkey" FOREIGN KEY ("A") REFERENCES "public"."Makanan"("idMakanan") ON DELETE CASCADE ON UPDATE CASCADE;
